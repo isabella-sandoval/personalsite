0fc0bf38ce3e625f02599b69685703e9a7de7a90
isabella sandoval <isabella.sandoval95@gmail.com>
2021-12-16 20:56:22 -0800

    shape animation

--- main.js
+++ main.js
@@ -24,6 +24,17 @@ scene.add(torus)
 torus.position.z = 30;
 torus.position.setX(-10);
 
+
+const geometry2 = new THREE.TorusGeometry(10, 3, 16, 100);
+const material2 = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus2 = new THREE.Mesh(geometry2, material2);
+
+scene.add(torus2)
+
+torus2.position.z = -30;
+torus2.position.setX(10);
+
+
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
@@ -40,7 +51,7 @@ const controls = new OrbitControls(camera, renderer.domElement)
 function addObjects(){
   const geometry = new THREE.SphereGeometry(0.25, 24,24);
   const material = new THREE.MeshStandardMaterial( {color: 0xffffff});
-  const obj = new THREE.Mesh( geometry, material);
+  const obj = new THREE.Mesh( geometry, material );
 
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
 
@@ -54,18 +65,6 @@ Array(30).fill().forEach(addObjects)
 const backgroundTexture = new THREE.TextureLoader().load('background-unsplash.jpg')
 scene.background = backgroundTexture;
 
-function animate(){
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  controls.update();
-
-  renderer.render(scene, camera);
-
-}
-
-animate()
 
 
 const bellaTexture = new THREE.TextureLoader().load('bella.jpg');
@@ -78,25 +77,37 @@ const bella = new THREE.Mesh(
 scene.add(bella);
 
 
-const moonTexture = new THREE.TextureLoader().load('splash.jpg');
+const shapeTexture = new THREE.TextureLoader().load('splash.jpg');
 const normalTexture = new THREE.TextureLoader().load('splash.jpg');
 
-const moon = new THREE.Mesh(
+const shape = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
-    map: moonTexture,
+    map: shapeTexture,
     normalMap: normalTexture,
   })
 );
 
-scene.add(moon);
+scene.add(shape);
+shape.position.z = 30;
+shape.position.setX(10);
 
-moon.position.z = 30;
-moon.position.setX(10);
+const shape2 = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: shapeTexture,
+    normalMap: normalTexture,
+  })
+);
+  
+  
+scene.add(shape2);
+shape2.position.z = -30;
+shape2.position.setX(-10);
 
 
 const conegeometry = new THREE.ConeGeometry( 5, 20, 32 );
-const conematerial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const conematerial = new THREE.MeshBasicMaterial( {color: 0xefc88b} );
 const cone = new THREE.Mesh( conegeometry, conematerial );
 scene.add( cone );
 
@@ -104,12 +115,61 @@ scene.add( cone );
 cone.position.z = 50;
 cone.position.setX(15);
 
+const cone2geometry = new THREE.ConeGeometry( 5, 20, 32 );
+const cone2material = new THREE.MeshBasicMaterial( {color: 0xefc88b} );
+const cone2 = new THREE.Mesh( cone2geometry, cone2material );
+scene.add( cone2 );
+
+
+cone2.position.z = -50;
+cone2.position.setX(-15);
+
 
 const cylindergeometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-const cylindermaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const cylindermaterial = new THREE.MeshBasicMaterial( {color: 0xeee5e9} );
 const cylinder = new THREE.Mesh( cylindergeometry, cylindermaterial );
 scene.add( cylinder );
 
-cylinder.position.z = -30;
-cylinder.position.setX(-30);
+cylinder.position.z = -50;
+cylinder.position.setX(-60);
 
+const cylinder2geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+const cylinder2material = new THREE.MeshBasicMaterial( {color: 0xeee5e9} );
+const cylinder2 = new THREE.Mesh( cylinder2geometry, cylinder2material );
+scene.add( cylinder2 );
+
+cylinder2.position.z = 50;
+cylinder2.position.setX(60);
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  torus2.rotation.x -= 0.01;
+  torus2.rotation.y -= 0.005;
+  torus2.rotation.z -= 0.01;
+
+  cone.rotation.x += 0.01;
+  cone.rotation.y += 0.005;
+  cone.rotation.z += 0.01;
+  cone2.rotation.x -= 0.01;
+  cone2.rotation.y -= 0.005;
+  cone2.rotation.z -= 0.01;
+
+  cylinder.rotation.x += 0.01;
+  cylinder.rotation.y += 0.005;
+  cylinder.rotation.z += 0.01;
+  cylinder2.rotation.x -= 0.01;
+  cylinder2.rotation.y -= 0.005;
+  cylinder2.rotation.z -= 0.01;
+
+
+  controls.update();
+
+  renderer.render(scene, camera);
+
+}
+
+animate()
